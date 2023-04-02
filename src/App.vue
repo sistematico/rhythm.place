31ef0255febbb1278fa93420e2d2814db84a4355
Lucas Saliés Brum <97509+sistematico@users.noreply.github.com>
2023-04-02 16:53:22 -0400

    Alterações:  M index.html

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Player from './components/player.vue'
 </script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a href="https://rhythm.place">
+      <img src="img/logo.svg" class="logo" alt="Rhythm Place" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Player />
 </template>
-
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
+  /* padding: 1.5em; */
   will-change: filter;
   transition: filter 300ms;
+ 
+  width: 100%;
+  max-width: 600px;
+  height: auto;
+
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+.logo:hover { filter: drop-shadow(0 0 2em #42b883aa); /* #646cffaa */ }
 </style>
