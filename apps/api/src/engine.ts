cbf4d407f60fb3db7c11a006cbcad329870e321b
Lucas Saliés Brum <sistematico@gmail.com>
2024-01-10 16:14:47 -0400

    Update

--- apps/api/src/engine.ts
+++ apps/api/src/engine.ts
@@ -6,7 +6,7 @@ if (!songsPath) throw new Error("SONGS_PATH not set");
 function randomFile(): string {
   const files = fg.globSync(songsPath + "/**/*.mp3", { absolute: true });
   const index = Math.round(Math.random() * (files.length - 1));
-  return "annotate:cover=cover.jpg" + files[index];
+  return "annotate:cover=cover.jpg:" + files[index];
 }
 
 Bun.write(Bun.stdout, randomFile());
